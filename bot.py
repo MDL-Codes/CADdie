4cfea7b4debbdd314d26a8498c4ecd5a11649ea5
Kylie Gun <kyliegun0@gmail.com>
2025-06-23 17:00:58 -0400

    setting up praise command

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is now running!")
+
+async def main():
+    async with bot:
+        await bot.load_extension("praise")  # matches praise.py
+        await bot.start(TOKEN)
+
+import asyncio
+asyncio.run(main())
